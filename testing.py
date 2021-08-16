0992e0c3f98472f0196f89fb39ebb3636806111b
daniellan2002 <52892124+daniellan2002@users.noreply.github.com>
2021-08-15 21:07:47 -0700

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,86 @@
+'''************************Testings***************************'''
+    # print()
+    # print('-----------------------------------------------------------')
+    # print()
+
+    '''Testing all_interesting_files()'''
+    #interesting_files.extend(all_interesting_files())
+    #print(interesting_files)
+
+
+    '''Testing files_by_name()'''
+    #interesting_files.extend(files_by_name(files_considered, 'README.md'))
+    #print(interesting_files)
+
+
+    '''Test files_by_ext()'''
+    #interesting_files.extend(files_by_ext(files_considered, '.oc'))
+    #print(interesting_files)
+    #print(len(interesting_files))
+
+
+    '''Test files_by_text()'''
+
+
+    # #files_by_text(files_considered, 'print')
+    #
+    #interesting_files.extend(files_by_text(files_considered, 'in'))
+    #for n in interesting_files:
+        #print(n)
+    #print(len(interesting_files))
+
+
+
+    '''Test less_than()'''
+
+    #interesting_files.extend(less_than(files_considered, 5000))
+
+    #for n in interesting_files:
+        #print(n + ':      ' + str(os.path.getsize(n)))
+    #print(len(interesting_files))
+
+
+
+    '''Test greater_than()'''
+
+    #interesting_files.extend(greater_than(files_considered, 5000))
+
+    #for n in interesting_files:
+        #print(n + ':      ' + str(os.path.getsize(n)))
+    #print(len(interesting_files))
+
+
+
+
+    '''Test first_line()'''
+
+    #first_line(interesting_files)
+
+
+
+    '''Test duplicate_file()'''
+    #duplicate_file(interesting_files)
+
+
+    '''Test change_time()'''
+    #change_time(interesting_files)
+
+
+
+
+
+
+
+    #directory_files(user_input)
+    #directory_files testing:
+    #files_considered.extend(directory_files(user_input))
+
+    #all_files testing:
+    #files_considered.extend(all_files(user_input))
+
+
+    #for n in files_considered:
+        #print(n)
+
+    #print_files() testing
+    #print_files(user_input)
